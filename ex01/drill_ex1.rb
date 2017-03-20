98911ab5bc04659574280209b759b5543c84c12f
Alex Law <achlawuk@gmail.com>
2017-03-20 15:51:38 +0000

    Updated drill_ex1.rb

--- ex01/drill_ex1.rb
+++ ex01/drill_ex1.rb
@@ -1,8 +1,11 @@
-puts "Hello World!"
-puts "Hello Again"
-puts "I like typing this."
-puts "This is fun."
-#puts "Yay!  Printing."
-puts "I'd much rather 'not'."
-puts 'I "said" do not touch this.'
+# puts "Hello World!"
+# puts "Hello Again"
+# puts "I like typing this."
+# puts "This is fun."
+# puts "Yay!  Printing."
+# puts "I'd much rather 'not'."
+# puts 'I "said" do not touch this.'
+
 puts "A new line of text"
+
+# This octothorpe turns a line into a comment.
