7ed9b3c3ed8a160a88bb627051b8117461ed3eb5
Alex Law <achlawuk@gmail.com>
2017-03-20 16:12:20 +0000

    Add ex3.rb, drill_ex3.rb and drill2_ex3.rb

---
+++ ex03/drill2_ex3.rb
@@ -0,0 +1,3 @@
+puts "How many KFC meals did I eat this month?"
+
+puts 4 + 5 + 6 * 2
---
+++ ex03/drill_ex3.rb
@@ -0,0 +1,35 @@
+# Prints the string.
+puts "I will now count my chickens:"
+
+# Prints the string with calculation result.
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# Prints the string with calculation result.
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# Prints the string.
+puts "Now I will count the eggs:"
+
+#Prints calculation result.
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+# Prints the string.
+puts "Is it true that 3 + 2 < 5 - 7?"
+# Prints calculation result.
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# Prints the string with calculation result.
+puts "What is 3 + 2? #{3.0 + 2.0}"
+# Prints the string with calculation result.
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+# Prints the string.
+puts "Oh, that's why it's false."
+
+# Prints the string.
+puts "How about some more."
+
+# Prints the string with either true of false.
+puts "Is it greater? #{5.0 > -2.0}"
+# Prints the string with either true of false.
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+# Prints the string with either true of false.
+puts "is it less or equal? #{5.0 <= -2.0}"
