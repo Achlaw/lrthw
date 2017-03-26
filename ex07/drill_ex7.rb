efe369b8f41301c22a11385f8d940439af9915e3
Alex Law <achlawuk@gmail.com>
2017-03-26 09:57:49 +0100

    ex07 complete

---
+++ ex07/drill_ex7.rb
@@ -0,0 +1,26 @@
+# Prints the string.
+puts "Mary had a little lamb."
+# Prints the string with the single quoted text in brackets.
+puts "Its fleece was white as #{'snow'}."
+# Prints the string.
+puts "And everywhere that Mary went."
+# Prints a dot ten times.
+puts "." * 10 # what'd that do?
+# Lines 10 to 21 are variables assigned with a string letter.
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+# Both of these lines prints out the variables associated with a string letter.
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
