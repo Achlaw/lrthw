8abe6b6b87ddc05f51d11636d29a628f2e0acaaa
Alex Law <achlawuk@gmail.com>
2017-03-26 14:49:58 +0100

    ex28 complete

---
+++ ex28/drill_ex28.rb
@@ -0,0 +1,12 @@
+=begin
+	
+	==		Equal or not.
+	!=		Not equal to.
+	>		Greather than.
+	<		Less than.
+	>=		Greater than or equal to.
+	<=		Less than or equal to.
+	<=>		Greater than, equals to or less than.
+	===		Use for testing equality within a clause statement.
+
+=end
