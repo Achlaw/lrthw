028df5ce8f85caaae485e0e7ad6f3c33bb06ac41
Alex Law <achlawuk@gmail.com>
2017-03-26 10:13:07 +0100

    ex10 complete

---
+++ ex10/drill_ex10.rb
@@ -0,0 +1,19 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat Food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+puts "A\ahairy\bcat."
+puts "A\rlazy\vcat."
+puts "A\fcatfish\ton a platter."
