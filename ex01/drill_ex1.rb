ecd6445a20106c9fe4871ab707bac9fb3e6c78e9
Alex Law <achlawuk@gmail.com>
2017-03-26 09:05:00 +0100

    ex01 complete

--- ex01/drill_ex1.rb
+++ ex01/drill_ex1.rb
@@ -8,4 +8,9 @@
 
 puts "A new line of text"
 
-# This octothorpe turns a line into a comment.
+=begin
+
+QUESTION: Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+ANSWER: # This octothorpe turns a line into a comment.
+
+=end
