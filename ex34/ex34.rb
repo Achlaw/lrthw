a51119a5bb10ce9f0be1c6dd4ac969c5c5ea553d
Alex Law <achlawuk@gmail.com>
2017-03-26 22:02:27 +0100

    Add ex34.rb

---
+++ ex34/ex34.rb
@@ -0,0 +1,13 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "The second (2nd) animal is at 1 and is a ruby."
+puts "The third (3rd) animal is at 2 and is a peacock."
+puts "The first (1st) animal is at 0 and is a bear."
+puts "The fourth (4th) animal is at 3 and is a kangaroo."
+puts "The firth (5th) animal is at 4 and is a whale."
+puts "The third (3rd) animal is at 2 and is a peacock."
+puts "The sixth (6th) animal is at 6 and is a platypus."
+puts "The fifth (5th) animal is at 4 and is a whale."
+
+def animal_name(string)
+end
